26fc8e2f18308addeb65fa0cdcfe584a5746937b
Bryan Alejandro Cubillos Prieto <bryant_alejandro@hotmail.com>
2025-07-14 20:02:01 -0500

    UI:WEB: Prepare scaffold

--- wtw_ui/src/app/config/app.config.server.ts
+++ wtw_ui/src/app/config/app.config.server.ts
@@ -1,8 +1,9 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
+
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { serverRoutes } from '../route/app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
