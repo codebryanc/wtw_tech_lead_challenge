764e3c2fc0c771febf239145466ceedefc8c19b9
Bryan Alejandro Cubillos Prieto <bryant_alejandro@hotmail.com>
2025-07-15 09:05:28 -0500

    UI:WEB: Filters on new component

--- wtw_ui/src/app/components/all-request/all-request.component.ts
+++ wtw_ui/src/app/components/all-request/all-request.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { RequestService } from '../../service/remote/request.service';
 import { RequestsEntity } from '../../models/requestsEntity';
 import { RequestCardComponent } from '../request-card/request-card.component';
-import { RequestTypeService } from '../../service/remote/request-type.service';
-import { RequestStatusService } from '../../service/remote/request-status.service';
-import { RequestStatusEntity } from '../../models/requestStatusEntity';
-import { RequestTypeEntity } from '../../models/requestTypeEntity';
+import { FilterRequestComponent } from '../filter-request/filter-request.component';
 import { RequestFilterDto } from '../../models/dtos/request.dto';
 
 @Component({
@@ -17,7 +13,7 @@ import { RequestFilterDto } from '../../models/dtos/request.dto';
   imports: [
     CommonModule,
     RequestCardComponent,
-    FormsModule
+    FilterRequestComponent
   ],
   templateUrl: './all-request.component.html',
   styleUrl: './all-request.component.scss'
@@ -29,26 +25,14 @@ export class AllRequestComponent implements OnInit {
   loading: boolean = false;
   error: string | null = null;
   
-  // Filter properties for UI only
-  statusFilter: string = '';
-  typeFilter: string = '';
-  searchText: string = '';
-  
-  // Available options for filters
-  availableStatuses: RequestStatusEntity[] = [];
-  availableTypes: RequestTypeEntity[] = [];
-
   constructor(
     private _requestService: RequestService,
-    private _requestTypeService : RequestTypeService,
-    private _requestStatusService : RequestStatusService,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       this.loadAllRequests();
-      this.getFilterOptions();
     }
   }
 
@@ -79,43 +63,11 @@ export class AllRequestComponent implements OnInit {
     return request.reqId;
   }
 
-  private getFilterOptions(): void {
-    this.availableStatuses = this._requestStatusService.getRequestStatus();
-    this.availableTypes = this._requestTypeService.getRequestTypes();
-  }
-
-  onFilterChange(): void {
-    // Check if searchText has minimum 3 characters when present
-    const hasValidSearch = !this.searchText || this.searchText.length >= 3;
-    
-    // If no filters are applied or search text is invalid, show all requests
-    if ((!this.statusFilter && !this.typeFilter && !this.searchText) || !hasValidSearch) {
+  onFiltersChanged(filter: RequestFilterDto): void {
+    // If filter is empty, show all requests
+    if (!filter || Object.keys(filter).length === 0) {
       this.filteredRequests = [...this.requests];
       return;
-    }
-
-    // Build filter object for backend
-    const filter: RequestFilterDto = {};
-
-    // Map selected values to IDs
-    if (this.statusFilter) {
-      const selectedStatus = this.availableStatuses.find(s => s.resId === this.statusFilter);
-      if (selectedStatus) {
-        filter.RequestStatusId = selectedStatus.resId;
-      }
-    }
-
-    if (this.typeFilter) {
-      const selectedType = this.availableTypes.find(t => t.rtyId === this.typeFilter);
-      if (selectedType) {
-        filter.RequestTypeId = selectedType.rtyId;
-      }
-    }
-
-    // For search text, use JsonProperty and JsonValue (only if 3+ characters)
-    if (this.searchText && this.searchText.length >= 3) {
-      filter.JsonProperty = 'data'; // Search in data property
-      filter.JsonValue = this.searchText;
     }
 
     // Call backend filtered requests
@@ -130,10 +82,7 @@ export class AllRequestComponent implements OnInit {
     });
   }
 
-  clearFilters(): void {
-    this.statusFilter = '';
-    this.typeFilter = '';
-    this.searchText = '';
+  onFiltersCleared(): void {
     this.filteredRequests = [...this.requests];
   }
 }
---
+++ wtw_ui/src/app/components/filter-request/filter-request.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { RequestTypeService } from '../../service/remote/request-type.service';
+import { RequestStatusService } from '../../service/remote/request-status.service';
+import { RequestStatusEntity } from '../../models/requestStatusEntity';
+import { RequestTypeEntity } from '../../models/requestTypeEntity';
+import { RequestFilterDto } from '../../models/dtos/request.dto';
+
+@Component({
+  selector: 'app-filter-request',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './filter-request.component.html',
+  styleUrl: './filter-request.component.scss'
+})
+export class FilterRequestComponent implements OnInit {
+  
+  @Output() filtersChanged = new EventEmitter<RequestFilterDto>();
+  @Output() filtersCleared = new EventEmitter<void>();
+
+  // Filter properties
+  statusFilter: string = '';
+  typeFilter: string = '';
+  searchText: string = '';
+  
+  // Available options for filters
+  availableStatuses: RequestStatusEntity[] = [];
+  availableTypes: RequestTypeEntity[] = [];
+
+  constructor(
+    private _requestTypeService: RequestTypeService,
+    private _requestStatusService: RequestStatusService
+  ) {}
+
+  ngOnInit(): void {
+    this.getFilterOptions();
+  }
+
+  private getFilterOptions(): void {
+    this.availableStatuses = this._requestStatusService.getRequestStatus();
+    this.availableTypes = this._requestTypeService.getRequestTypes();
+  }
+
+  onFilterChange(): void {
+    // Check if searchText has minimum 3 characters when present
+    const hasValidSearch = !this.searchText || this.searchText.length >= 3;
+    
+    // If no filters are applied or search text is invalid, emit empty filter
+    if ((!this.statusFilter && !this.typeFilter && !this.searchText) || !hasValidSearch) {
+      this.filtersChanged.emit({});
+      return;
+    }
+
+    // Build filter object for backend
+    const filter: RequestFilterDto = {};
+
+    // Map selected values to IDs
+    if (this.statusFilter) {
+      const selectedStatus = this.availableStatuses.find(s => s.resId === this.statusFilter);
+      if (selectedStatus) {
+        filter.RequestStatusId = selectedStatus.resId;
+      }
+    }
+
+    if (this.typeFilter) {
+      const selectedType = this.availableTypes.find(t => t.rtyId === this.typeFilter);
+      if (selectedType) {
+        filter.RequestTypeId = selectedType.rtyId;
+      }
+    }
+
+    // For search text, use JsonProperty and JsonValue (only if 3+ characters)
+    if (this.searchText && this.searchText.length >= 3) {
+      filter.JsonProperty = 'data'; // Search in data property
+      filter.JsonValue = this.searchText;
+    }
+
+    // Emit the filter to parent component
+    this.filtersChanged.emit(filter);
+  }
+
+  clearFilters(): void {
+    this.statusFilter = '';
+    this.typeFilter = '';
+    this.searchText = '';
+    this.filtersCleared.emit();
+  }
+}
